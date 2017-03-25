a7a99e4f96e5d9ef556188d93c97a799fc7a8228
stridtul <stridtul@abv.bg>
2017-03-25 17:50:13 +0200

    test

--- test/public/index.php
+++ test/public/index.php
@@ -1,64 +1,50 @@
 <?php
-
 use Phalcon\Loader;
+use Phalcon\Tag;
+use Phalcon\Mvc\Url;
 use Phalcon\Mvc\View;
 use Phalcon\Mvc\Application;
-use Phalcon\Di\FactoryDefault;
-use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\DI\FactoryDefault;
 use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
-
-
-
-// Register an autoloader
-$loader = new Loader();
-
-$loader->registerDirs(
-    [
-        "../app/controllers/",
-        "../app/models/",
-    ]
-);
-
-$loader->register();
-
-
-
-// Create a DI
-$di = new FactoryDefault();
-
-// Setup the view component
-$di->set(
-    "view",
-    function () {
-        $view = new View();
-
-        $view->setViewsDir("../app/views/");
-
-        return $view;
-    }
-);
-
-// Setup a base URI so that all generated URIs include the "tutorial" folder
-$di->set(
-    "url",
-    function () {
-        $url = new UrlProvider();
-
-        $url->setBaseUri("/starships/test/");
-
-        return $url;
-    }
-);
-
-
-
-$application = new Application($di);
-
 try {
+    // Register an autoloader
+    $loader = new Loader();
+    $loader->registerDirs(
+        array(
+            '../app/controllers/',
+            '../app/models/'
+        )
+    )->register();
+    // Create a DI
+    $di = new FactoryDefault();
+    // Set the database service
+    $di['db'] = function() {
+        return new DbAdapter(array(
+            "host"     => "localhost",
+            "username" => "root",
+            "password" => "secret",
+            "dbname"   => "tutorial"
+        ));
+    };
+    // Setting up the view component
+    $di['view'] = function() {
+        $view = new View();
+        $view->setViewsDir('../app/views/');
+        return $view;
+    };
+    // Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di['url'] = function() {
+        $url = new Url();
+        $url->setBaseUri('/test/');
+        return $url;
+    };
+    // Setup the tag helpers
+    $di['tag'] = function() {
+        return new Tag();
+    };
     // Handle the request
-    $response = $application->handle();
-
-    $response->send();
-} catch (\Exception $e) {
+    $application = new Application($di);
+    echo $application->handle()->getContent();
+} catch (Exception $e) {
     echo "Exception: ", $e->getMessage();
 }
