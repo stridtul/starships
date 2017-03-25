bab646738c52163d4eb15caa6cb8a3fc5cd3994f
stridtul <stridtul@abv.bg>
2017-03-25 14:05:57 +0200

    test

---
+++ test/public/index.php
@@ -0,0 +1,64 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+
+
+
+// Register an autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        "../app/controllers/",
+        "../app/models/",
+    ]
+);
+
+$loader->register();
+
+
+
+// Create a DI
+$di = new FactoryDefault();
+
+// Setup the view component
+$di->set(
+    "view",
+    function () {
+        $view = new View();
+
+        $view->setViewsDir("../app/views/");
+
+        return $view;
+    }
+);
+
+// Setup a base URI so that all generated URIs include the "tutorial" folder
+$di->set(
+    "url",
+    function () {
+        $url = new UrlProvider();
+
+        $url->setBaseUri("/tutorial/");
+
+        return $url;
+    }
+);
+
+
+
+$application = new Application($di);
+
+try {
+    // Handle the request
+    $response = $application->handle();
+
+    $response->send();
+} catch (\Exception $e) {
+    echo "Exception: ", $e->getMessage();
+}
